0a1febcba841cc404f9f9f169e00a0fc6b718da3
h4rky <79790068+h4rky@users.noreply.github.com>
2024-12-28 12:03:44 +0100

    Replace checkboxes with svg rectangles wrapped in div tags

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 let done_count = 1;
 
-let body = document.getElementById("container");
+const box_not_done = `<svg width="17" height="17"><rect width="17" height="17" rx="2" ry="2" fill="#c8c2ae"/></svg>`
+const box_done = `<svg width="17" height="17"><rect width="17" height="17" rx="2" ry="2" fill="#57a773"/></svg>`
+let checkbox_container = document.getElementById("checkbox-container");
 
 let array = Array(done_count).fill(true);
 let array_not_done = Array(100 - done_count).fill(false);
 array = array.concat(array_not_done);
 
 array.forEach(element => {
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    if (element) {checkbox.setAttribute("checked", "true");}
-    checkbox.setAttribute("disabled", "true");
-    body.appendChild(checkbox);
-    console.log(element);
+    let checkbox = document.createElement("div");
+    checkbox.setAttribute("class", "checkbox");
+    
+    if (element) {
+        checkbox.innerHTML = box_done;
+    } else {
+        checkbox.innerHTML = box_not_done;
+    }
+
+    checkbox_container.appendChild(checkbox);
 });
