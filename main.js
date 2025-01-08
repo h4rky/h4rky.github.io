767a2fe9c92e8e55d52b3b6f5afaae4195c5dad0
h4rky <79790068+h4rky@users.noreply.github.com>
2025-01-08 08:39:16 +0100

    5th thing done - third version of the do 100 things page

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const DONE_COUNT = 4;
+const DONE_COUNT = 5;
 
 let container_checkbox = document.getElementById("checkbox-container");
 let container_log = document.getElementById("log-container");
