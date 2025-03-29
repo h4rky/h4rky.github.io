47b47dc240ef184b6535ce014720f1eea21f1e3d
h4rky <79790068+h4rky@users.noreply.github.com>
2025-03-29 10:49:45 +0100

    16 thing read kometen kommer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const DONE_COUNT = 15;
+const DONE_COUNT = 16;
 
 let container_checkbox = document.getElementById("checkbox-container");
 let container_log = document.getElementById("log-container");
