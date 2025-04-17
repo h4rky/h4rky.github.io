72e2ee1c8bca7acecb8c062fbb78c5fb2ee5489e
h4rky <79790068+h4rky@users.noreply.github.com>
2025-04-17 10:30:34 +0200

    18 thing autodarkmode + flexoki colour scheme

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const DONE_COUNT = 17;
+const DONE_COUNT = 18;
 
 let container_checkbox = document.getElementById("checkbox-container");
 let container_log = document.getElementById("log-container");
