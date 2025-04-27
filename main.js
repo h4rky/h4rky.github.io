59b4c4c060dba18022878e31d0de8ef2eb691fff
h4rky <79790068+h4rky@users.noreply.github.com>
2025-04-27 17:38:21 +0200

    21 and 22 thing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const DONE_COUNT = 20;
+const DONE_COUNT = 22;
 
 let container_checkbox = document.getElementById("checkbox-container");
 let container_log = document.getElementById("log-container");
